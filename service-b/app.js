85e6ea6b6a562bdde0a73acc5941d755eb2235f3
bathula,manu <bathulamanohar25@gmail.com>
2026-04-22 13:39:55 +0530

    Update app.js

--- service-b/app.js
+++ service-b/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Service B');
+  res.send('Hello manohar from Service B');
 });
 
 app.listen(5001, () => {
