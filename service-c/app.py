c43c40cc96b2805d553763f573c325e63bea8b6c
bathula,manu <bathulamanohar25@gmail.com>
2026-04-22 16:15:05 +0530

    Update app.py

--- service-c/app.py
+++ service-c/app.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/service-c')   # ✅ ADD THIS
+@app.route('/service-c')
 def home():
-    return "Hello from rug Service C"
+    return "Hello from Service C"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
